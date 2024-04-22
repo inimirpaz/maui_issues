698f750c1559579f2797d75fe2bc84762873fc16
Mirco Pazzaglia <mirco.pazzaglia@inim.biz>
2024-04-22 16:24:27 +0200

    first commit

---
+++ ToolbarItemBindingIssue/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ToolbarItemBindingIssue;
+
+public partial class MainPage : ContentPage
+{
+    private bool _isVisible = true;
+    public bool IsVisible { get => _isVisible; set { _isVisible = value; OnPropertyChanged(); } }
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        MainPageVM viewModel = (MainPageVM)BindingContext;
+        viewModel.IsVisible = !viewModel.IsVisible;
+        IsVisible = !IsVisible;
+    }
+}
---
+++ ToolbarItemBindingIssue/MainPageVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ToolbarItemBindingIssue;
+
+public class MainPageVM : INotifyPropertyChanged
+{
+    private bool _isVisible = true;
+    public bool IsVisible { get => _isVisible; set { _isVisible = value; OnPropertyChanged(); } }
+
+    public MainPageVM()
+    {
+    }
+
+#pragma warning disable CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChangedEventHandler changed = PropertyChanged;
+        if (changed == null)
+            return;
+        changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+#pragma warning restore CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+}
