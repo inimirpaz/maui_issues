f7d045d067ef6fd98bfb340335ff364f5ac8bba5
Mirco Pazzaglia <mirco.pazzaglia@inim.biz>
2024-05-29 14:22:33 +0200

    Add: repro

--- ToolbarItemBindingIssue/MainPage.xaml.cs
+++ ToolbarItemBindingIssue/MainPage.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace ToolbarItemBindingIssue;
+﻿using System.Windows.Input;
+
+namespace ToolbarItemBindingIssue;
 
 public partial class MainPage : ContentPage
 {
+    public ICommand SelectedCommand { get; private set; }
+
     public MainPage()
     {
+        SelectedCommand = new Command((c) =>
+        {
+            if (collView.SelectedItem != null)
+            {
+                if (collView.SelectedItem is MainPageVM.MyModel item)
+                {
+                    System.Diagnostics.Debug.WriteLine(item.Title, "INFO");
+                    collView.SelectedItem = null;
+                }
+            }
+        }, s => !IsBusy);
+
         InitializeComponent();
     }
 }
--- XamarinToolbarItemBindingIssue/XamarinToolbarItemBindingIssue/XamarinToolbarItemBindingIssue/MainPage.xaml.cs
+++ XamarinToolbarItemBindingIssue/XamarinToolbarItemBindingIssue/XamarinToolbarItemBindingIssue/MainPage.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace XamarinToolbarItemBindingIssue
 {
     public partial class MainPage : ContentPage
     {
+        public ICommand SelectedCommand { get; private set; }
+
         public MainPage()
         {
+            SelectedCommand = new Command((c) =>
+            {
+                if (collView.SelectedItem != null)
+                {
+                    if (collView.SelectedItem is MainPageVM.MyModel item)
+                    {
+                        System.Diagnostics.Debug.WriteLine(item.Title, "INFO");
+                        collView.SelectedItem = null;
+                    }
+                }
+            }, s => !IsBusy);
+
             InitializeComponent();
         }
     }
